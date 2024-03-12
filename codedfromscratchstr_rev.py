c45c0e181cfab660a475acdfe937c3fc0401e50b
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-12 16:59:21 -0400

    modifying the code for funsies; beths tricks

--- codedfromscratchstr_rev.py
+++ codedfromscratchstr_rev.py
@@ -8,5 +8,7 @@ Created on Tue Mar 12 16:50:01 2024
 
 str_back = "Reverse the string"
 str_rev = (str_back[::-1])
-print(str_rev)
+
+for char in str_rev:
+    print(char, end = ',')
 #lmao I actually coded this from scratch. seems to work

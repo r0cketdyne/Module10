72a13f16d96b7ac87ab61136a2aef613ff6c2493
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-11 23:33:16 -0400

    written from scratch

---
+++ WRITTENFROMSCRATCH.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 23:23:30 2024
+
+@author: stephenson
+"""
+
+fuck = input("fuck put in any var. writing this dumbass program again")
+
+def thissucks(fuck):
+ 
+    count1 = fuck.count('A')
+    count2 = fuck.count('E')
+    count3 = fuck.count('I')
+    count4 = fuck.count('O')
+    count5 = fuck.count('U')
+    
+    print(count1 + count2 + count3 + count4 + count5)
+    
+    return
+
+thissucks(fuck)
+print(thissucks)

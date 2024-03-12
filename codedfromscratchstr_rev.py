366e29cc9c899adb414039aeced25fd1284191de
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-12 17:04:27 -0400

    behold, keyval pairs?

--- codedfromscratchstr_rev.py
+++ codedfromscratchstr_rev.py
@@ -9,8 +9,8 @@ Created on Tue Mar 12 16:50:01 2024
 str_back = "Reverse the string"
 str_rev = (str_back[::-1])
 
-for char in str_rev:
-    print(char, end = ',')
+for index, nextletter in enumerate(str_rev): 
+    print(index, nextletter)
 #lmao I actually coded this from scratch. seems to work
 
 #CAN ENUMERATE BE USED TO PRINT KEY VALUE PAIRS????? YEEEET

bd280c1687ca311e9ffb69fbde5006d2182f05ec
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-12 17:00:39 -0400

    keyvalpairs

--- codedfromscratchstr_rev.py
+++ codedfromscratchstr_rev.py
@@ -12,3 +12,5 @@ str_rev = (str_back[::-1])
 for char in str_rev:
     print(char, end = ',')
 #lmao I actually coded this from scratch. seems to work
+
+#CAN ENUMERATE BE USED TO PRINT KEY VALUE PAIRS????? YEEEET

8a135579f065a73f480f6782bd6630a8263eb6a6
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-12 16:53:12 -0400

    bleh

---
+++ codedfromscratchstr_rev.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 16:50:01 2024
+
+@author: stephenson
+"""
+
+str_back = "Reverse the string"
+str_rev = (str_back[::-1])
+print(str_rev)
+#lmao I actually coded this from scratch. seems to work

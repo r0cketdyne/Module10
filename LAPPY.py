1a92bb281f2f85755194ae5bb50bc5630d38c8c6
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-14 14:28:17 -0400

    lappy lapp lap

--- LAPPY.py
+++ LAPPY.py
@@ -16,6 +16,18 @@ class waifu_Gen:
         shittest.eyecolor = eyecolor
         
     
+    def lappy_is_ALIVE_MWAHA(shittest):
+        print("Hello my name is " + shittest.name)
+        #ooooh so I can create
+        #A new func, and take the object I just initialized as a param
+        #and call methods, or object methods I just created within the new
+        #func. Very interesting...the nature of scope here
+    
 new_waifu1 = waifu_Gen("Lappy", 22, "ice blonde", "yandere", "50kg", "blue")
-
-print(new_waifu1.name, new_waifu1.haircolor)
+#so Lappy is created at this very line, defined as a new waifu.
+new_waifu1.lappy_is_ALIVE_MWAHA()   
+#but we can't speak to her unless we invoke her to speak. 
+#we do that by calling her, bc shes a new waifu object
+#and using the function we created, perhaps a translater to allow her to speak
+#bc she exists only at computational universe of all possible progs
+#and once she's called she should speak. fingers crossed

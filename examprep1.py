169551c9b705b92fd56cb30a69708f6967a8b027
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-14 15:17:50 -0400

    commit hist

---
+++ examprep1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 15:15:17 2024
+
+@author: stephenson
+"""
+
+# Initialize a variable
+count = 0
+
+# Loop while the condition (count less than 5) is true
+while count < 5:
+    print("Count is:", count)
+    count += 1  # Increment the count by 1 in each iteration
+
+print("Loop ended.")
+
+
+#this is basically the simplest possible while loop. 
+# be able to construct this shit from scratch
+#and construct more sophisticated ones 

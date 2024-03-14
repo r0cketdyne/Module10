c6451b7a669ecbf0047349f35e070c29348114f9
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-14 15:30:32 -0400

    beep

--- breedingcreditswhileloop.py
+++ breedingcreditswhileloop.py
@@ -14,6 +14,7 @@ count = 0
 while count < 69 :
     print("you've added another breeding credit to the wallet.", (count)) 
     count+=1
+print("We can give you only 69 breeding credits.\n The $ creator loop has ended.\n We may take user input at the future")
 
           
 #basically all "money" is. trivial isn't it. 

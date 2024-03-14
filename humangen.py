e12f977b9ce7fb7cde5722c5598bc28447dbe16d
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-14 13:57:55 -0400

    practice creating classes. in this case a human gen thingy

--- humangen.py
+++ humangen.py
@@ -13,10 +13,13 @@ class HumanGen:
         self.namaewa = namaewa
         self.age = age
         self.hobby = hobby
+    def __str__(self):
+        return f"{self.namae}{self.age}"
         
 
 
-
 newdude1 = HumanGen("Matthew", 4669, "Parkour")
+newdude2 = HumanGen("Dinosaur", 22, "Eating")
+newdude3 = HumanGen("Dorsal_Fin", 21, "Being Sus")
 
-print(newdude1.namaewa)
+print(newdude1.namaewa, newdude2.age, newdude3.namaewa)

98f30f81eea823f9e3813d5adb0348fa0c084c9a
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-14 15:35:14 -0400

    for loops python simple exam prep

---
+++ forloopexamprep.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 15:34:41 2024
+
+@author: stephenson
+"""
+
+#hyper simple for loops python
+
+# Define a sequence (in this case, a list)
+my_list = [1, 2, 3, 4, 5]
+
+# Loop over each element in the sequence
+for element in my_list:
+    # Print each element
+    print("Current element:", element)
+
+# Print after the loop ends
+print("Loop ended.")

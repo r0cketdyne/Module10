53f1b4326ff352e1f3258bd0790763ab8b1c63d9
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-14 15:38:55 -0400

    why is the arr here called and tagges as a list

--- forloopexamprep.py
+++ forloopexamprep.py
@@ -9,12 +9,21 @@ Created on Thu Mar 14 15:34:41 2024
 #hyper simple for loops python
 
 # Define a sequence (in this case, a list)
-my_list = [1, 2, 3, 4, 5]
+my_list = [1, 2, 3, 4, 5] #list is initialized, which isn't really a list
+#it is actually an array of INTS. ffs
+
 
 # Loop over each element in the sequence
 for element in my_list:
+    #so, the general syntax for looping over shit is for, array element, 
+    #in the array name "stupidly called my list, despite being an arr
+    
     # Print each element
     print("Current element:", element)
 
 # Print after the loop ends
 print("Loop ended.")
+
+
+
+

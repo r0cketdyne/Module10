67b9cff52eaf48bb50673c49578b1341f3d8a8c2
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-14 13:51:59 -0400

    EE

---
+++ humangen.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 13:43:55 2024
+
+@author: stephenson
+"""
+
+#we are gonna create a class that generated humans
+
+class HumanGen:
+    def __init__(self, namaewa, age, hobby):
+        self.namaewa = namaewa
+        self.age = age
+        self.hobby = hobby
+        
+
+
+
+newdude1 = HumanGen("Matthew", 4669, "Parkour")
+
+print(newdude1.namaewa)

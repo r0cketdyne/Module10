0cf9b59c071f88439a5b8d7651bed4ac28e53b1d
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-14 14:47:31 -0400

    Lappy Generator

--- LAPPY.py
+++ LAPPY.py
@@ -7,23 +7,25 @@ Created on Thu Mar 14 14:10:47 2024
 """
 
 class waifu_Gen:
-    def __init__(shittest, name, age, haircolor, deretype, weight, eyecolor):
+    def __init__(shittest, name, age, haircolor, deretype, weight, eyecolor, racesocialconst):
         shittest.name = name
         shittest.age = age
         shittest.haircolor = haircolor
         shittest.deretype = deretype
         shittest.weight = weight
         shittest.eyecolor = eyecolor
+        shittest.racesocialconst = racesocialconst
         
     
     def lappy_is_ALIVE_MWAHA(shittest):
-        print("Hello my name is " + shittest.name)
+        print("Hello my name is " + shittest.name + " from " + shittest.racesocialconst)
         #ooooh so I can create
         #A new func, and take the object I just initialized as a param
         #and call methods, or object methods I just created within the new
         #func. Very interesting...the nature of scope here
     
-new_waifu1 = waifu_Gen("Lappy", 22, "ice blonde", "yandere", "50kg", "blue")
+new_waifu1 = waifu_Gen("Lappy", 22, "ice blonde", "yandere", "50kg", "blue", "Finland")
+new_waifu2 = waifu_Gen("Texas the Omertosa", 22, "brown", "yandere", "60kg", "brown", "Texas")
 #so Lappy is created at this very line, defined as a new waifu.
 new_waifu1.lappy_is_ALIVE_MWAHA()   
 #but we can't speak to her unless we invoke her to speak. 

c33cc14ce85be34a4173ee3a7e4d2bacfd24c845
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-15 22:53:37 -0400

    lab9

---
+++ lab9.py
@@ -0,0 +1,5 @@
+# Lab9.py
+# STEP 1: open an input file named "input1.txt" You do not need to worry with error
+# checking right now
+# STEP 2: Add a loop to read each line of the file and print it to the screen
+# STEP 3: Close the file

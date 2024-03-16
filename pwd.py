32dcf1d81b26f1cf49ec51ee9374665286874b9a
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-16 00:40:37 -0400

    dude I did it. this makes sense. finally

---
+++ pwd.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 16 00:32:40 2024
+
+@author: stephenson
+"""
+
+#ffs. I'm going to open some rando file. what is even pwd
+
+fml = open("/Users/stephenson/Desktop/Module10/freiren.txt", "r")
+
+ugh = fml.readline()
+print(ugh)

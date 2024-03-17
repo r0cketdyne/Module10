340e3c41c1174f9be180c2d1a983afb2336ce49a
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-16 20:03:27 -0400

    fromscratchlab9onenightonlyyy

---
+++ Lab9scratch.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 16 20:02:38 2024
+
+@author: stephenson
+"""
+
+# Lab9.py
+# STEP 1: open an input file named "input1.txt" You do not need to worry with error
+# checking right now
+# STEP 2: Add a loop to read each line of the file and print it to the screen
+# STEP 3: Close the file

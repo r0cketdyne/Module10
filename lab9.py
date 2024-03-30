24c6c868000a2f7217370ebe7cad0ef758a040ab
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-03-30 13:59:20 -0400

    Lab9 late. IM TRAVELING RN LOL

--- lab9.py
+++ lab9.py
@@ -1,5 +1,27 @@
-# Lab9.py
-# STEP 1: open an input file named "input1.txt" You do not need to worry with error
-# checking right now
-# STEP 2: Add a loop to read each line of the file and print it to the screen
-# STEP 3: Close the file
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 13:53:25 2024
+
+@author: stephenson
+"""
+
+# Initialize a variable to count the lines containing numbers
+numeric_lines_count = 0
+#init var, numeric_lines_count to count lines with nums, lines 3 4 and 5
+# Open the file named "input1.txt"
+with open("input1.txt", "r") as file:
+# Open the file named "input1.txt", r means read, r is one of 3 possibilities here
+    for line in file:
+ #this loop reads each line at input1.txt
+        print(line.strip())
+#log the actual lines to terminal emulator
+
+        if any(char.isdigit() for char in line.strip()):
+# Check if the line contains a numeric value, you'll add one to the count initialized at line 10
+            numeric_lines_count += 1
+# += is adding to the var alloc'd at the stack at line 10 by count 1
+
+
+print("There are {} lines containing numbers in this data file.".format(numeric_lines_count))
+#logging count of lines containing nums to terminal emulator
